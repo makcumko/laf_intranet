6435e3ea77d425848ed815e6a6bd122c7b5c29a4
Makc @ laf <m.kulesh@gmail.com>
2013-06-24 16:24:28 +0400

    wiki logo

--- www/wiki/LocalSettings.php
+++ www/wiki/LocalSettings.php
@@ -37,7 +37,9 @@ $wgStylePath = "$wgScriptPath/skins";
 
 ## The relative URL path to the logo.  Make sure you change this from the default,
 ## or else you'll overwrite your logo when you upgrade!
-$wgLogo             = "$wgStylePath/common/images/wiki.png";
+$wgLogo             = "$wgStylePath/images/logo.jpg";
+//echo $wgLogo;
+//die();
 
 ## UPO means: this is also a user preference option
 
